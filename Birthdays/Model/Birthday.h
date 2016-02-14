e6aeec2cb0859957be52e47a78672151b9afffe8
irfanlone <irfan.mf69@gmail.com>
2016-02-14 13:40:24 -0800

    Fixed Local Directory structure

---
+++ Birthdays/Model/Birthday.h
@@ -0,0 +1,22 @@
+//
+//  Birthday.h
+//  Birthdays
+//
+//  Created by Irfan Lone on 11/10/15.
+//  Copyright © 2015 Irfan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Birthday : NSManagedObject
+
+// Insert code here to declare functionality of your managed object subclass
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "Birthday+CoreDataProperties.h"
